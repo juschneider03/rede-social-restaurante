4c58470ba6c802da13c62b9ff3e8e48f78839a02
Juliana Schneider <juliana.schneider@paytrack.com.br>
2023-09-29 17:44:52 -0300

    Rebranding do projeto

---
+++ controller/FormPost.js
@@ -0,0 +1,80 @@
+export class FormPost {
+    constructor(idForm, idTextarea, listPost) {
+      this.form = document.getElementById(idForm);
+      this.textarea = document.getElementById(idTextarea);
+      this.listPost = document.getElementById(listPost);
+      this.addSubmit();
+    }
+  
+    formValidate(value) {
+      if (
+        value === "" ||
+        value === null ||
+        value === undefined ||
+        value.length < 3
+      ) {
+        return false;
+      }
+      return true;
+    }
+  
+    onSubmit(func) {
+      this.form.addEventListener("submit", func);
+    }
+  
+    addSubmit() {
+      const handleSubmit = (event) => {
+        event.preventDefault(); 
+        console.log("evento adicionado")
+        if (this.formValidate(this.textarea.value)) {
+          const newPost = document.createElement('li');
+          newPost.classList.add('post');
+          const time = this.getTime();
+          newPost.innerHTML = `
+          <div class="infoUserPost">
+            <div class="imgUserPost"></div>
+  
+            <div class="nameAndHour">
+              <strong>Douglas Pujol</strong>
+              <p>${time}</p>
+            </div>
+          </div>
+  
+          <p>
+           ${this.textarea.value}
+          </p>
+  
+          <div class="actionBtnPost">
+            <button type="button" class="filesPost like">
+              <img src="./assets/heart.svg" alt="Curtir">
+              Curtir
+            </button>
+            <button type="button" class="filesPost comment">
+              <img src="./assets/comment.svg" alt="Comentar">
+              Comentar
+            </button>
+            <button type="button" class="filesPost share">
+              <img src="./assets/share.svg" alt="Compartilhar">
+              Compartilhar
+            </button>
+          </div>
+          `;
+          this.listPost.appendChild(newPost);
+          this.textarea.value = "";
+        } else {
+          alert("ALGUM ERRO");
+        }
+      };
+  
+      this.onSubmit(handleSubmit);
+    }
+  
+    getTime() {
+      const time = new Date();
+      const hour = time.getHours();
+      const minutes = time.getMinutes();
+      return `${hour}h ${minutes}min`;
+    }
+  }
+  
+  const postForm = new FormPost("formPost", "textarea", "posts");
