22a972344b9bba23938352113fe21ae69a4c3204
Juliana Schneider <juliana.schneider@paytrack.com.br>
2023-10-24 20:04:48 -0300

    PHP login

--- controller/Login.js
+++ controller/Login.js
@@ -1,11 +1,28 @@
-class Login {
-    
-    validarFormulario() {
-        var email = document.getElementById("email").value;
-        var senha = document.getElementById("senha").value;
+function fazerLogin() {
+  var email = document.getElementById('email').value;
+  var senha = document.getElementById('senha').value;
 
-        if (email.trim() === "" || senha.trim() === "") {
-            alert("Preencha todos os campos.");
-        } 
-    }
+  // Validar se os campos estão preenchidos
+  if (email === '' || senha === '') {
+      alert('Preencha todos os campos');
+      return;
+  }
+
+  // Enviar os dados para o servidor via AJAX
+  $.ajax({
+      type: 'POST',
+      url: 'verificar_login.php',
+      data: {
+          email: email,
+          senha: senha
+      },
+      success: function(response) {
+          if (response === 'success') {
+              // Redirecionar para a página de sucesso
+              window.location.href = 'index.html';
+          } else {
+              alert('Credenciais inválidas. Tente novamente.');
+          }
+      }
+  });
 }
---
+++ php/verificar_login.php
@@ -0,0 +1,29 @@
+<?php
+    $hostname = "localhost";
+    $bancodedados = "bd_restaurante";
+    $usuario = "root";
+    $senha = "";
+
+    $conn = new mysqli($host, $usuario, $senha, $banco);
+
+    if ($conn->connect_error) {
+        die("Conexão ao banco de dados falhou: " . $conn->connect_error);
+    }
+    
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    
+    $query = "SELECT * FROM tabela_login WHERE email = '$email' AND senha = '$senha'";
+    $result = $conn->query($query);
+    
+    if ($result->num_rows == 1) {
+        header("Location: index.html");
+        exit();
+    } else {
+        echo 'error'; 
+    }
+    
+    $conn->close();
+?>
+
+
